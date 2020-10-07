b8b58788875ebf2f2bdc76a3631155917df0f26e
Carina_Mir <alt.carin@gmail.com>
2020-10-08 02:28:31 +0300

    commit #2

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
+
 	$str_browser_language = !empty($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? strtok(strip_tags($_SERVER['HTTP_ACCEPT_LANGUAGE']), ',') : '';
 	$str_browser_language = !empty($_GET['language']) ? $_GET['language'] : $str_browser_language;
+
 	switch (substr($str_browser_language, 0,2))
 	{
 		case 'de':
